767b0da5bbccee3c7f9480d9f6089780c0675ffe
Junior Cintra <junior.developer@outlook.com.br>
2022-01-01 17:25:36 -0300

    content-type

--- cliente/android.js
+++ cliente/android.js
@@ -4,14 +4,25 @@ var opcoes = {
   hostname: "localhost",
   porta: 80,
   path: "/",
+  method: "post",
   headers: {
     Accept: "application/json",
+    // "Content-type": "application/x-www-form-urlencoded",
+    "Content-type": "application/json",
   },
 };
 
+//Content-type
+var html = "nome=Jose";
+var json = {
+  nome: "Jose",
+};
+
+var string_json = JSON.stringify(json);
+
 var buffer_corpo_response = [];
 
-http.get(opcoes, function (res) {
+var req = http.request(opcoes, function (res) {
   res.on("data", function (pedaco) {
     buffer_corpo_response.push(pedaco);
   });
@@ -20,6 +31,8 @@ http.get(opcoes, function (res) {
     var corpo_response = Buffer.concat(buffer_corpo_response).toString();
     console.log(corpo_response);
   });
-
-  res.on("error", function () {});
 });
+
+req.write(string_json);
+
+req.end();
